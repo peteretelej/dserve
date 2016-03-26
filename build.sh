d432378cfdc037db21eb4e21ee3e40e9668bf30f
Peter Etelej <peter@etelej.com>
2016-03-26 20:53:42 +0300

    Ship releases as compressed archives

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ GOOS=windows go build -o releases/dserve.exe main.go && \
 echo "Windows and Linux executable binaries built. Located in releases folder." && \
 zip -r releases/dserve_for_windows.zip releases/dserve.exe && \
 tar -cvzf releases/dserve_for_linux.tar.gz releases/dserve && \
+rm releases/dserve releases/dserve.exe
 echo "Release builds completed"
