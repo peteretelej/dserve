9a7377d5182fb6c71be2d83f1883dc67bee12f06
Peter Etelej <peter@etelej.com>
2016-03-31 09:30:20 +0300

    Prep 32 bit build of release

--- build.sh
+++ build.sh
@@ -2,8 +2,13 @@
 rm -rf releases/*
 GOOS=linux go build -o releases/dserve main.go && \
 GOOS=windows go build -o releases/dserve.exe main.go && \
-echo "Windows and Linux executable binaries built. Located in releases folder." && \
+echo "Windows and Linux executable binaries built. Located in releases folder."
+zip -r releases/dserve_for_windowsx64.zip releases/dserve.exe && \
+tar -cvzf releases/dserve_for_linux_amd64.tar.gz releases/dserve && \
+rm releases/dserve releases/dserve.exe
+rm -rf releases/dserve.exe
+GOARCH=386 GOOS=windows go build -o releases/dserve.exe main.go && \
+echo "Windows 32bit executable binaries built. Located in releases folder."
 zip -r releases/dserve_for_windows.zip releases/dserve.exe && \
-tar -cvzf releases/dserve_for_linux.tar.gz releases/dserve && \
 rm releases/dserve releases/dserve.exe
 echo "Release builds completed"
