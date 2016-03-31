4397e2a454fb43ea7aff82d3af2907d0092207c1
Peter Etelej <peter.etelej@gmail.com>
2016-03-31 09:38:04 +0300

    Add windows 32bit release

--- build.sh
+++ build.sh
@@ -5,8 +5,7 @@ GOOS=windows go build -o releases/dserve.exe main.go && \
 echo "Windows and Linux executable binaries built. Located in releases folder."
 zip -r releases/dserve_for_windowsx64.zip releases/dserve.exe && \
 tar -cvzf releases/dserve_for_linux_amd64.tar.gz releases/dserve && \
-rm releases/dserve releases/dserve.exe
-rm -rf releases/dserve.exe
+rm releases/dserve.exe
 GOARCH=386 GOOS=windows go build -o releases/dserve.exe main.go && \
 echo "Windows 32bit executable binaries built. Located in releases folder."
 zip -r releases/dserve_for_windows.zip releases/dserve.exe && \
