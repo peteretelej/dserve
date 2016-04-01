1b21cbe94117499798fc6bb19077aa026e92fe7e
Peter Etelej <peter.etelej@gmail.com>
2016-04-01 10:50:53 +0300

    Add linux 386 and Mac darwin/386 builds

--- build.sh
+++ build.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
+set -e
+
 rm -rf releases/*
+
+# windows and linux x64
+echo "Building Linuxamd64, Windowsx64"
 GOOS=linux go build -o releases/dserve main.go && \
 GOOS=windows go build -o releases/dserve.exe main.go && \
-echo "Windows and Linux executable binaries built. Located in releases folder."
 zip -r releases/dserve_for_windowsx64.zip releases/dserve.exe && \
 tar -cvzf releases/dserve_for_linux_amd64.tar.gz releases/dserve && \
-rm releases/dserve.exe
+rm releases/dserve{,.exe}
+
+# Windows 32bit
+echo "Building Windows, Linux 32 bit"
+GOARCH=386 GOOS=linux go build -o releases/dserve main.go && \
 GOARCH=386 GOOS=windows go build -o releases/dserve.exe main.go && \
-echo "Windows 32bit executable binaries built. Located in releases folder."
 zip -r releases/dserve_for_windows.zip releases/dserve.exe && \
-rm releases/dserve releases/dserve.exe
-echo "Release builds completed"
+tar -cvzf releases/dserve_for_linux.tar.gz releases/dserve && \
+rm releases/dserve{,.exe}
+
+# Mac, iOS 386 (darwin/386)
+echo "Building for Mac"
+GOARCH=386 GOOS=darwin go build -o releases/dserve main.go && \
+zip -r releases/dserve_for_mac.zip releases/dserve && \
+rm releases/dserve
+
+echo "BUILDS completed. ================="
