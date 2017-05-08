fdeccc20d02145b890c2faa3580125b40238c6f3
Peter Etelej <peter@etelej.com>
2017-05-08 16:46:58 +0300

    Add test for validBasicAuth

--- main_test.go
+++ main_test.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"encoding/base64"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 var fakeFSHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("fs")) })
+
+func TestValidBasicAuth(t *testing.T) {
+	username, password := "tester", "pass123"
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Authorization",
+		fmt.Sprintf("%s %s", username, base64.StdEncoding.EncodeToString([]byte(password))))
+	if valid := validBasicAuth(req); valid {
+		t.Errorf("validbasicauth got %v want %v", valid, false)
+	}
+
+}
 
 func TestBasicAuth(t *testing.T) {
 	req, err := http.NewRequest("GET", "/", nil)
